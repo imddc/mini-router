c6e7bed678c8eac764597115d529f854cd490548
itsimddc <easyhoo2426@163.com>
2025-12-09 00:14:31 +0800

    feat(router): 添加路由功能并重构计数器组件模板
    - 引入 LibRouter 和 createWebHistory 实现路由功能
    - 重构计数器组件模板，增加语义化 section 结构
    - 将路由状态管理与计数器状态绑定，实现动态视图更新

--- packages/prepare/src/main.ts
+++ packages/prepare/src/main.ts
@@ -1,6 +1,7 @@
 import { effect, signal } from 'alien-signals';
 import { renderCounter } from './components/Counter';
 import { renderLinks } from './components/Header';
+import { createWebHistory, LibRouter } from './lib/router';
 import { $ } from './utils/$';
 
 const count = signal(0);
@@ -22,27 +23,41 @@ const routes = [
   {
     path: '/counter1',
     component: {
-      template: ({ count }) => `<div>count: ${count}</div>`
+      template: ({ count }: { count: number }) => `
+      <section>
+        <h1>this is section 1</h1>
+        <div>count2: ${count}</div>
+      </section>
+      `
     }
   },
   {
     path: '/counter2',
     component: {
-      template: ({ count }) => `<div>count2: ${count}</div>`
+      template: ({ count }: { count: number }) => `
+      <section>
+        <h1>this is section 2</h1>
+        <div>count2: ${count}</div>
+      </section>
+      `
     }
   }
 ];
 
-window.addEventListener('popstate', (e) => {
-  console.log('e => ', e)
-})
+// router start
+const router = new LibRouter({
+  history: createWebHistory(),
+  routes
+});
 
-renderLinks(links);
+router
 
+renderLinks({ links, router });
+
+// 这里是为了 让count变化时，router-view也变化
 effect(() => {
-  const route = routes.find((route) => route.path === location.pathname);
+  const route = router.currentRoute;
+  console.log('currentRoute => ', route);
   $('.router-view')!.innerHTML =
-    route?.component.template({
-      count: count()
-    }) || '';
+    route?.component?.template({ count: count() }) || '';
 });
