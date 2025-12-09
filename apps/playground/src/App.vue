fb6d2f977cee5316351cd783380c8360cde23253
itsimddc <easyhoo2426@163.com>
2025-12-10 02:08:47 +0800

    feat(playground): 初始化 Vue 3 + TypeScript + Vite 项目
    添加 playground 项目基础结构，包括配置文件、路由设置和样式

---
+++ apps/playground/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li v-for="link in links" :key="link.path">
+                        <router-link :to="link.path">{{ link.label }}</router-link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <router-view />
+    </div>
+</template>
+
+<script setup lang="ts">
+const links = [
+    {
+        path: '/',
+        label: 'index'
+    },
+    {
+        path: '/about',
+        label: 'about'
+    }
+];
+</script>
