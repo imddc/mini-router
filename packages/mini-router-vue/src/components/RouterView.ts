3d61dbbd0e86a1eef0665e6f5bbd4713a7ad7503
itsimddc <easyhoo2426@163.com>
2025-12-10 13:15:55 +0800

    feat(RouterView): 添加路由注入和当前路由组件渲染功能
    实现 RouterView 组件通过注入获取路由实例，并渲染当前路由对应的组件。添加对路由变化的监听和默认插槽支持。

--- packages/mini-router-vue/src/components/RouterView.ts
+++ packages/mini-router-vue/src/components/RouterView.ts
@@ -1,9 +1,30 @@
-import { defineComponent, h } from 'vue';
+import { computed, defineComponent, h, inject, watchEffect } from 'vue';
+import { ROUTER_KEY } from '../history/common';
 
-const RouterView = defineComponent({
-    setup(_, { slots }) {
-        return () => h('template', {}, [slots.defaults?.()]);
+const RouterView = defineComponent(
+    (_, { slots }) => {
+        const router = inject(ROUTER_KEY);
+        if (!router) {
+            console.error('RouterView 组件必须在 Router 组件内部使用');
+            return () => h('template', {}, []);
+        }
+
+        const currentRoute = computed(() => router.currentRoute.value);
+
+        watchEffect(() => {
+            console.log('currentRoute.value?.component => ', currentRoute.value?.component);
+        });
+
+        return () => {
+            return slots.default?.({
+                Component: currentRoute.value?.component
+            });
+        };
+    },
+    {
+        props: {},
+        slots: {}
     }
-});
+);
 
 export { RouterView };
