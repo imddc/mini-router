d604f762792b0ca1bfdf74252127b9802692ec10
itsimddc <easyhoo2426@163.com>
2025-12-10 13:15:49 +0800

    feat(router-link): 添加路由跳转功能并验证router注入
    为RouterLink组件添加to属性用于指定跳转路径
    在点击时阻止默认行为并通过router.push进行路由跳转
    添加router注入验证，未找到时输出警告信息

--- packages/mini-router-vue/src/components/RouterLink.ts
+++ packages/mini-router-vue/src/components/RouterLink.ts
@@ -1,9 +1,31 @@
-import { defineComponent, h } from 'vue';
+import { defineComponent, h, inject } from 'vue';
+import { ROUTER_KEY } from '../history/common';
+import { warn } from '../warn';
 
 const RouterLink = defineComponent({
+    props: {
+        to: {
+            type: String,
+            required: true
+        }
+    },
     setup(props, { slots }) {
-        console.log('props => ', props);
-        return () => h('a', {}, slots.default?.());
+        const router = inject(ROUTER_KEY);
+        if (!router) {
+            warn('请确保 mini-router-vue 已安装');
+        }
+
+        return () =>
+            h(
+                'a',
+                {
+                    onClick: (e: Event) => {
+                        e.preventDefault();
+                        router?.push(props.to);
+                    }
+                },
+                slots.default?.()
+            );
     }
 });
 
