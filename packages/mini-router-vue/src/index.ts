1531b9c24ad0614eb26d1900bbf8b59a204d6345
itsimddc <easyhoo2426@163.com>
2025-12-10 13:15:38 +0800

    feat(router): 导出 RouterLink 和 RouterView 组件

--- packages/mini-router-vue/src/index.ts
+++ packages/mini-router-vue/src/index.ts
@@ -1,4 +1,9 @@
 // 统一具名导出所有模块，作为库的公开 API
 
+import { RouterLink } from './components/RouterLink';
+import { RouterView } from './components/RouterView';
+
 export * from './history';
 export * from './router';
+
+export { RouterLink, RouterView };
