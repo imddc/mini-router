74215bf5d353c28a77b55c983c55f8fd2369df8e
itsimddc <easyhoo2426@163.com>
2025-12-11 17:54:24 +0800

    feat(utils): 添加判断对象是否为 Promise 的辅助函数

--- packages/mini-router-vue/src/utils/index.ts
+++ packages/mini-router-vue/src/utils/index.ts
@@ -3,4 +3,13 @@
  */
 const assign = Object.assign;
 
-export { assign };
+/**
+ * 用于判断一个对象是否为 Promise
+ * @param obj 要判断的对象
+ * @returns 如果是 Promise 则返回 true，否则返回 false
+ */
+function isPromise(obj: any) {
+    return obj && typeof obj.then === 'function';
+}
+
+export { assign, isPromise };
